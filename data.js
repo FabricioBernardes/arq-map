698709c623606c8de21e5527b8260fa169cbae24
Fabricio Bernardes <fabricio.bernardes@vnda.com.br>
2023-07-14 18:23:11 -0300

    adiciona campo de id nos dados de sítios

--- data.js
+++ data.js
@@ -1,5 +1,6 @@
 window.data = [
     {
+        "id": 1,
         "ref map": "Pernigotti e Almeida (1961)",
         "Per. e Alm": "6 Casa Abandonada",
         "E. Brasil": "86",
@@ -7,6 +8,7 @@ window.data = [
         "lat": -32.1412345
     },
     {
+        "id": 2,
         "ref map": "Pernigotti e Almeida (1961)",
         "Per. e Alm": "5 Morro Grande",
         "E. Brasil": "92",
@@ -14,12 +16,14 @@ window.data = [
         "lat": -32.1503656
     },
     {
+        "id": 3,
         "ref map": "Pernigotti e Almeida (1961); Freddy",
         "Per. e Alm": "7 Eucaliptos",
         "long": -52.1867409,
         "lat": -32.1349039
     },
     {
+        "id": 4,
         "ref map": "Pernigotti e Almeida (1961); Naue 1971; Naue 1971 lab",
         "Per. e Alm": "8 Vieira",
         "Naue1971Lb": "33",
@@ -30,6 +34,7 @@ window.data = [
         "lat": -32.1056686
     },
     {
+        "id": 5,
         "ref map": "Freitas (2005)",
         "Freit.2005": "RS-LS-38 Sport Clube Rio Grande",
         "n cat lpn": "130 -> 144, 149",
@@ -38,6 +43,7 @@ window.data = [
         "lat": -32.0930521
     },
     {
+        "id": 6,
         "ref map": "Freitas (2005)",
         "Freit.2005": "RS-LS-37 Renato Frasca Cândido",
         "n cat lpn": "129",
@@ -46,6 +52,7 @@ window.data = [
         "lat": -32.0974316
     },
     {
+        "id": 7,
         "ref map": "Freitas (2005)",
         "Freit.2005": "RS-LS-36 Ferro Velho",
         "n cat lpn": "128",
@@ -54,6 +61,7 @@ window.data = [
         "lat": -32.0824012
     },
     {
+        "id": 8,
         "ref map": "Freitas (2005)",
         "Freit.2005": "RS-LS-35 Maximo Pereira da Silva",
         "n cat lpn": "124 -> 127",
@@ -62,6 +70,7 @@ window.data = [
         "lat": -32.0863442
     },
     {
+        "id": 9,
         "ref map": "Freitas (2005)",
         "Freit.2005": "RS-LS-34 João José Guedes",
         "n cat lpn": "121, 122, 123",
@@ -70,6 +79,7 @@ window.data = [
         "lat": -32.0857273
     },
     {
+        "id": 10,
         "ref map": "Freitas (2005)",
         "Freit.2005": "RS-LS-33 Adi Oppa Rodrigues",
         "n cat lpn": "120",
@@ -77,7 +87,8 @@ window.data = [
         "long": -52.1515853,
         "lat": -32.0770233
     },
-    {
+    {   
+        "id": 11,
         "ref map": "Freitas (2005)",
         "Freit.2005": "RS-LS-32 Adão Silva",
         "n cat lpn": "117, 118, 119, 147",
@@ -86,6 +97,7 @@ window.data = [
         "lat": -32.0833399
     },
     {
+        "id": 12,
         "ref map": "Freitas (2005)",
         "Freit.2005": "RS-LS-31 Zeferino Pereira  ERRADO \/ CONFERIR LEPAN",
         "n cat lpn": "116; 145, 146, 152 -> 171",
@@ -94,6 +106,7 @@ window.data = [
         "lat": -32.07541
     },
     {
+        "id": 13,
         "ref map": "Freitas (2005)",
         "Freit.2005": "RS-LS-29 Ivon Silva",
         "n cat lpn": "113; 114",
@@ -102,6 +115,7 @@ window.data = [
         "lat": -32.0709275
     },
     {
+        "id": 14,
         "ref map": "Pernigotti e Almeida (1961); Freitas (2005)",
         "Per. e Alm": "9 Rampa do vitorino",
         "Naue1971Lb": "15",
@@ -109,6 +123,7 @@ window.data = [
         "lat": -32.0872244
     },
     {
+        "id": 15,
         "ref map": "Freitas (2005)",
         "Naue1971Ar": "sitio 32",
         "Freit.2005": "RS-LS-01 Marcelino Amaral Brancão",
@@ -119,6 +134,7 @@ window.data = [
         "lat": -32.0302723
     },
     {
+        "id": 16,
         "ref map": "Freitas (2005); Naue (1971); Schmitz 1976",
         "Naue1971Ar": "sitios 22\/23\/24",
         "Freit.2005": "RS-LS-24 Dunas",
@@ -128,12 +144,14 @@ window.data = [
         "lat": -31.8599759
     },
     {
+        "id": 17,
         "ref map": "Schmitz (1976)",
         "Schmitz": "RS-RG-50",
         "long": -52.265969,
         "lat": -31.8518664
     },
     {
+        "id": 18,
         "ref map": "Schmitz (1976)",
         "Schmitz": "RS-RG-20",
         "Naue1971Ar": "Sitio 20",
@@ -142,17 +160,20 @@ window.data = [
         "lat": -31.8374396
     },
     {
+        "id": 19,
         "ref map": "Schmitz (1976)",
         "Schmitz": "S\/n  (RS-RG-20)",
         "long": -52.2797525,
         "lat": -31.8353971
     },
     {
+        "id": 20,
         "ref map": "Intercambio e Geopolitica",
         "long": -52.3005503,
         "lat": -31.8644637
     },
     {
+        "id": 21,
         "ref map": "Naue (1971)",
         "Naue1971Ar": "Sitio 36",
         "Freit.2005": "RS-LS-46 José Pedro Leggermann",
@@ -162,16 +183,19 @@ window.data = [
         "lat": -31.9302094
     },
     {
+        "id": 22,
         "ref map": "Intercambio e Geopolitica",
         "long": -52.30329,
         "lat": -31.8747528
     },
     {
+        "id": 23,
         "ref map": "Cleiton",
         "long": -52.2858769,
         "lat": -31.9743843
     },
     {
+        "id": 24,
         "ref map": "Schimitz (1976)",
         "Schmitz": "RS-RG-28",
         "Naue1971Ar": "Sitio 28",
@@ -180,40 +204,47 @@ window.data = [
         "lat": -31.9886039
     },
     {
+        "id": 25,
         "ref map": "Schmitz (1976)",
         "Schmitz": "RS-RG-10",
         "long": -52.2460053,
         "lat": -31.986193
     },
     {
+        "id": 26,
         "ref map": "Schimitz (1976)",
         "Schmitz": "S\/n RS-RG-10",
         "long": -52.2437707,
         "lat": -31.9814211
     },
     {
+        "id": 27,
         "ref map": "Intercambio e Geopolitica",
         "long": -52.2654495,
         "lat": -31.8493024
     },
     {
+        "id": 28,
         "ref map": "Schmitz (1976)",
         "Schmitz": "RS-RG-49",
         "long": -52.2636329,
         "lat": -31.850549
     },
     {
+        "id": 29,
         "ref map": "Intercambio e Geopolitica",
         "long": -52.2639883,
         "lat": -31.8465214
     },
     {
+        "id": 30,
         "ref map": "Schimitz (1976)",
         "Schmitz": "S\/n (RS-RG-49)",
         "long": -52.2606506,
         "lat": -31.8528314
     },
     {
+        "id": 31,
         "ref map": "Intercambio e Geopolitica",
         "Freit.2005": "RS-LS-11 Ariano Souza",
         "n cat lpn": "35 -> 40 ; 64, 65; 67; 453; 562;",
@@ -222,6 +253,7 @@ window.data = [
         "lat": -31.855125
     },
     {
+        "id": 32,
         "ref map": "Intercambio e Geopolitica",
         "Freit.2005": "RS-LS-65 Ariano Souza",
         "n cat lpn": "453",
@@ -230,42 +262,50 @@ window.data = [
         "lat": -31.8549267
     },
     {
+        "id": 33,
         "ref map": "Intercambio e Geopolitica",
         "long": -52.2587511,
         "lat": -31.8569203
     },
     {
+        "id": 34,
         "ref map": "Intercambio e Geopolitica",
         "long": -52.2578438,
         "lat": -31.8586577
     },
     {
+        "id": 35,
         "ref map": "Intercambio e Geopolitica",
         "long": -52.2580921,
         "lat": -31.8599777
     },
     {
+        "id": 36,
         "ref map": "Intercambio e Geopolitica",
         "long": -52.2580026,
         "lat": -31.8616591
     },
     {
+        "id": 37,
         "ref map": "Intercambio e Geopolitica",
         "long": -52.2572968,
         "lat": -31.8634898
     },
     {
+        "id": 38,
         "ref map": "Intercambio e Geopolitica",
         "E. Brasil": "90",
         "long": -52.2560363,
         "lat": -31.8675211
     },
     {
+        "id": 39,
         "ref map": "Fabricio",
         "long": -52.2543639,
         "lat": -31.8673429
     },
     {
+        "id": 40,
         "ref map": "Fabricio",
         "Naue1971Ar": "sitio 35",
         "E. Brasil": "404",
@@ -273,16 +313,19 @@ window.data = [
         "lat": -31.8789662
     },
     {
+        "id": 41,
         "ref map": "Fabricio",
         "long": -52.2791265,
         "lat": -31.848028
     },
     {
+        "id": 42,
         "ref map": "Cleiton",
         "long": -52.1953635,
         "lat": -32.0098502
     },
     {
+        "id": 43,
         "ref map": "Schimitz (1976)",
         "Schmitz": "RS-RG-21",
         "Freit.2005": "RS-LS-85 Oscar Erocildo Abreu",
@@ -292,6 +335,7 @@ window.data = [
         "lat": -31.8614284
     },
     {
+        "id": 44,
         "ref map": "Schmitz (1976)",
         "Schmitz": "RS-RG-03",
         "Naue1971Ar": "sitio 3 b",
@@ -300,6 +344,7 @@ window.data = [
         "lat": -32.0120266
     },
     {
+        "id": 45,
         "ref map": "Schmitz (1976)",
         "Schmitz": "RS-RG-03",
         "Naue1971Ar": "Sitio 3 a",
@@ -308,6 +353,7 @@ window.data = [
         "lat": -32.0125914
     },
     {
+        "id": 46,
         "ref map": "Schmitz (1976)",
         "Schmitz": "RS-RG-04",
         "Naue1971Ar": "Sitio 4",
@@ -316,6 +362,7 @@ window.data = [
         "lat": -32.0507129
     },
     {
+        "id": 47,
         "ref map": "Schmitz (1976); E. Brasil",
         "Schmitz": "RS-RG-14",
         "Naue1971Ar": "Sitio 14",
@@ -324,6 +371,7 @@ window.data = [
         "lat": -31.9951212
     },
     {
+        "id": 48,
         "ref map": "Schmitz (1976); E. Brasil",
         "Schmitz": "RS-RG-8",
         "Naue1971Ar": "Sitio 8",
@@ -332,12 +380,14 @@ window.data = [
         "lat": -31.9926502
     },
     {
+        "id": 49,
         "Naue1971Ar": "Sitio 2 c",
         "E. Brasil": "403",
         "long": -52.2398362,
         "lat": -31.9178603
     },
     {
+        "id": 50,
         "ref map": "Naue 1971art; Brasil",
         "Naue1971Ar": "Sitio 2 a",
         "E. Brasil": "RS 402",
@@ -345,6 +395,7 @@ window.data = [
         "lat": -31.9109702
     },
     {
+        "id": 51,
         "ref map": "Freitas (2005)",
         "Freit.2005": "RS-LS-49 Antonio Carlos Rocha Cabreira",
         "n cat lpn": "179",
@@ -353,6 +404,7 @@ window.data = [
         "lat": -31.9851015
     },
     {
+        "id": 52,
         "ref map": "Freitas 2005",
         "Freit.2005": "RS-LS-91",
         "LEPAN RSLS": "RS-LS-91",
@@ -360,23 +412,27 @@ window.data = [
         "lat": -31.984295
     },
     {
+        "id": 53,
         "LEPAN RSLS": "RS-LS-79",
         "long": -52.1256205,
         "lat": -31.9824691
     },
     {
+        "id": 54,
         "ref map": "Freitas 2005",
         "Freit.2005": "RS-LS-60 Korea",
         "long": -52.118464,
         "lat": -31.9855716
     },
     {
+        "id": 55,
         "ref map": "Hidraulica",
         "E. Brasil": "85",
         "long": -52.1264928,
         "lat": -32.0497055
     },
     {
+        "id": 56,
         "ref map": "Brasil (1997); Naue (1971art)",
         "Naue1971Ar": "Sitio 12a",
         "E. Brasil": "255",
@@ -384,6 +440,7 @@ window.data = [
         "lat": -31.8570966
     },
     {
+        "id": 57,
         "Naue1971Ar": "sitio 33",
         "Freit.2005": "RS-LS-02 Clube dos Caçadores do Rio Grande",
         "E. Brasil": "414",
@@ -391,12 +448,14 @@ window.data = [
         "lat": -32.1084951
     },
     {
+        "id": 58,
         "ref map": "Schmitz 1976",
         "Schmitz": "RS-RG-48",
         "long": -52.2572775,
         "lat": -31.8237564
     },
     {
+        "id": 59,
         "ref map": "Freitas",
         "Freit.2005": "RS-LS-68-Lixão",
         "LEPAN RSLS": "RS-LS-68-Lixão",
@@ -404,6 +463,7 @@ window.data = [
         "lat": -32.0727507
     },
     {
+        "id": 60,
         "ref map": "Freitas 2005",
         "Freit.2005": "RS-LS-93 Claudiomiro Figueiredo Portanova 2",
         "LEPAN RSLS": "RS-LS-93 Claudiomiro Figueiredo Portanova 2",
@@ -411,6 +471,7 @@ window.data = [
         "lat": -32.0179569
     },
     {
+        "id": 61,
         "ref map": "Freitas",
         "Freit.2005": "RS-LS-48 Claudiomiro Figueiredo Portanova 1",
         "LEPAN RSLS": "RS-LS-48 Claudiomiro Figueiredo Portanova 1",
@@ -418,58 +479,72 @@ window.data = [
         "lat": -32.0189163
     },
     {
+        "id": 62,
         "long": -52.2319957,
         "lat": -31.9023699
     },
     {
+        "id": 63,
         "long": -52.2311811,
         "lat": -31.9040957
     },
     {
+        "id": 64,
         "long": -52.2246702,
         "lat": -31.9209642
     },
     {
+        "id": 65,
         "long": -52.2318726,
         "lat": -31.920116
     },
     {
+        "id": 66,
         "long": -52.2251181,
         "lat": -31.9172537
     },
     {
+        "id": 67,
         "long": -52.2259737,
         "lat": -31.9151759
     },
     {
+        "id": 68,
         "long": -52.2265206,
         "lat": -31.9130685
     },
     {
+        "id": 69,
         "long": -52.2250248,
         "lat": -31.9233271
     },
     {
+        "id": 70,
         "long": -52.2277875,
         "lat": -31.9092182
     },
     {
+        "id": 71,
         "long": -52.2285374,
         "lat": -31.9079558
     },
     {
+        "id": 72,
         "long": -52.2372704,
         "lat": -31.9026447
     },
     {
+        "id": 73,
         "long": -52.2350035,
         "lat": -31.9044248
     },
     {
+        "id": 74,
         "long": -52.2232227,
         "lat": -31.9184977
     },
     {
+        "id": 75,
         "long": -52.224638,
         "lat": -31.9188404
     }
